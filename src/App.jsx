b9fedfa622fc25b6e662db9d440897dad2a12a01
Taehyeungkim <kth1837@snu.ac.kr>
2025-04-05 04:00:07 +0900

    :sparkles:after react-hook-step1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,75 @@
 import "./App.css";
-
+import { useState } from "react";
 const MAX_GUESTS_PER_STAFF = 2;
 const MAX_STAFFS_PER_MANAGER = 2;
 
 function App() {
+  const [guestCount, setGuestCount] = useState(0);
+  const [managerCount, setManagerCount] = useState(1);
+  const [staffCount, setStaffCount] = useState(1);
+
+  const incrementManagerCount = () => {
+    setManagerCount(managerCount + 1);
+  };
+
+  const decrementManagerCount = () => {
+    setManagerCount(managerCount - 1);
+  };
+
+  const incrementStaffCount = () => {
+    setStaffCount(staffCount + 1);
+  };
+
+  const decrementStaffCount = () => {
+    setStaffCount(staffCount - 1);
+  };
+
+  const incrementGuestCount = () => {
+    setGuestCount(guestCount + 1);
+  };
+
+  const decrementGuestCount = () => {
+    setGuestCount(guestCount - 1);
+  };
+
+  if (guestCount > staffCount * MAX_GUESTS_PER_STAFF) {
+    console.log("More staff needed");
+  } else {
+    console.log("No need for more staff");
+  }
+
+  if (staffCount > managerCount * MAX_STAFFS_PER_MANAGER) {
+    console.log("More manager needed");
+  } else {
+    console.log("No need for more manager");
+  }
   return (
     <div className="App">
       <div className="ControlContainer">
         <div className="Control">
           <div>
-            <h2>Managers: {1}</h2>
+            <h2>Managers: {managerCount}</h2>
             <div>
-              <button>Increment</button>
-              <button>Decrement</button>
+              <button onClick={incrementManagerCount}>Increment</button>
+              <button onClick={decrementManagerCount}>Decrement</button>
             </div>
           </div>
         </div>
         <div className="Control">
           <div>
-            <h2>Staffs: {1}</h2>
+            <h2>Staffs: {staffCount}</h2>
             <div>
-              <button>Increment</button>
-              <button>Decrement</button>
+              <button onClick={incrementStaffCount}>Increment</button>
+              <button onClick={decrementStaffCount}>Decrement</button>
             </div>
           </div>
         </div>
         <div className="Control">
           <div>
-            <h2>Guests: {1}</h2>
+            <h2>Guests: {guestCount}</h2>
             <div>
-              <button>Increment</button>
-              <button>Decrement</button>
+              <button onClick={incrementGuestCount}>Increment</button>
+              <button onClick={decrementGuestCount}>Decrement</button>
             </div>
           </div>
         </div>
